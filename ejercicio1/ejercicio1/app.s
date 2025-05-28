9c73566d6051cbcf51036a12bcbe7f23e737d921
joaquinprado <joaquin.prado@mi.unc.edu.ar>
2025-05-27 22:51:02 -0300

    Update app.s
    Comenze con las ventanitas

--- ejercicio1/ejercicio1/app.s
+++ ejercicio1/ejercicio1/app.s
@@ -12,6 +12,44 @@ main:
  	mov x20, x0	// Guarda la dirección base del framebuffer en x20
 	//---------------- CODE HERE ------------------------------------
 
+    // COLOR_EDIFICIO_BASE2 #8b8d81
+    movz w10, 0x8b, lsl 16
+    movk w10, 0x8d81, lsl 00
+
+    mov x1, 72
+    mov x2, 164 
+    mov x3, 49
+    mov x4, 388
+    mov x5, 210
+    bl dibujar_lineas_repetidasx
+
+    mov x1, 72
+    mov x2, 164 
+    mov x3, 278
+    mov x4, 141
+    mov x5, 23
+    bl dibujar_lineas_repetidasy
+
+    mov x1, 48
+    mov x2, 373
+    mov x3, 252
+    mov x4, 388
+    mov x5, 15
+
+    bl dibujar_lineas_repetidasy
+
+
+
+    //color negro para dar forma nomas
+    movz w10, 0x00, lsl 16
+    movk w10, 0x0000, lsl 00
+    mov x1, 60
+    mov x2, 164    
+    mov x3, 39
+    mov x4, 388
+    mov x5, 10
+    bl dibujar_lineas_repetidasx
+
     // COLOR_EDIFICIO_SOMBRA4 (la sombra del pilar que sobresale pero la de que mira para abajo)
     movz w10, 0x60, lsl 16
     movk w10, 0x5E48, lsl 00
@@ -36,6 +74,7 @@ main:
     mov x4,211
     mov x5,6
     bl dibujar_lineas_repetidasy
+
 
     // COLOR_EDIFICIO_SOMBRA3 (la sombra del pilar que sobresale)
     movz w10, 0x86, lsl 16
@@ -71,10 +110,83 @@ main:
     mov x5,4
     bl dibujar_lineas_repetidasx
 
+    mov x1,337
+    mov x2,192
+    mov x3,331
+    mov x4,395
+    mov x5,4
+    bl dibujar_lineas_repetidasx
+
+    mov x1, 386
+    mov x2, 394
+    mov x3, 389
+    mov x4, 212
+    mov x5, 5
+    bl dibujar_lineas_repetidasx
+
+
+
+    //COLOR_EDIFICIO_SOMBRA5
+    movz w10, 0x75, lsl 16
+    movk w10, 0x6d57, lsl 00    
+
+    mov x1,347
+    mov x2,330
+    mov x3,387
+    mov x4,337
+    mov x5,7
+    bl dibujar_lineas_repetidasy
+
+    mov x1, 280
+    mov x2, 318
+    mov x3, 332
+    mov x4, 327
+    mov x5, 8
+    bl dibujar_lineas_repetidasy
+
+    // Ventanas cerradas en EDIFICIO_BASE2 (color de rejas 9caec8,segundo color de rejas #7a8393, color de la parte de abajo 4f565f)
+
+    movz w10, 0x9C, lsl 16
+    movk w10, 0xAEC8, lsl 00
+
+    mov x1, 83
+    mov x2, 182
+    mov x3, 80
+    mov x4, 214
+    mov x5, 28
+    bl dibujar_lineas_intercaladasx
+
+    movz w10, 0x7A, lsl 16
+    movk w10, 0x8393, lsl 00
+
+    mov x1, 82
+    mov x2, 182
+    mov x3, 79
+    mov x4, 214
+    mov x5, 28
+    bl dibujar_lineas_intercaladasx
+
+    movz w10, 0x4F, lsl 16
+    movk w10, 0x565F, lsl 00
+    mov x1, 80
+    mov x2, 215
+    mov x3, 106
+    mov x4, 213
+    mov x5, 2
+    bl dibujar_lineas_repetidasy
+
+    movz w10, 0x8B, lsl 16
+    movk w10, 0x8D81, lsl 00
+    mov x1, 82
+    mov x2, 182
+    mov x3, 109
+    mov x4, 180
+    mov x5, 3
+    bl dibujar_lineas_repetidasy
+
     // COLOR_EDIFICIO_BASE
     movz w10, 0xCF, lsl 16
     movk w10, 0xCFCE, lsl 00
-
 
 
     mov x1,152
@@ -136,15 +248,24 @@ main:
     mov x5, 18
     bl dibujar_lineas_repetidasy
 
+    mov x1,344
+    mov x2,379
+    mov x3,385
+    mov x4,379
+    mov x5,15
+    bl dibujar_lineas_repetidasy
+
+    mov x1, 391
+    mov x2,394
+    mov x3, 394
+    mov x4, 212
+    mov x5, 6
+    bl dibujar_lineas_repetidasx
+
     movz w10, 0x60, lsl 16
     movk w10, 0x5E48, lsl 00
 
-    mov x1,337
-    mov x2,192
-    mov x3,331
-    mov x4,395
-    mov x5,4
-    bl dibujar_lineas_repetidasx
+
 
     mov x1,351
     mov x2,199
@@ -152,6 +273,7 @@ main:
     mov x4,216
     mov x5,5
     bl dibujar_lineas_repetidasy
+
 
 
 	// Ejemplo de uso de gpios
