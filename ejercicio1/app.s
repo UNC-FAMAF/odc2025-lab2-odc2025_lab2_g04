b5b18dc1f919033c0af8da0125feccb2a6c20c43
juani mancini <juan.mancini@mi.unc.edu.ar>
2025-06-01 21:31:27 +0000

    matched main

--- ejercicio1/app.s
+++ ejercicio1/app.s
@@ -16,26 +16,6 @@ main:
     movz w10, 0x2F, lsl 16 
     movk w10, 0x59A1, lsl 00
 
-<<<<<<< HEAD
-	mov x2, SCREEN_HEIGH         // Y Size
-loop1:
-	mov x1, SCREEN_WIDTH         // X Size
-loop0:
-	stur w10,[x0]  // Colorear el pixel N
-	add x0,x0,4	   // Siguiente pixel
-	// set color en w12
-	movz w12, #0xff
-	movk w12, #0xffff, lsl 16
-
-	//set n pixel en x13
-	mov x13, x20
-	add x13, x13, #128
-	stur w12, [x13] // Colorear el pixel x13
-	sub x1,x1,1	   // Decrementar contador X
-	cbnz x1,loop0  // Si no terminó la fila, salto
-	sub x2,x2,1	   // Decrementar contador Y
-	cbnz x2,loop1  // Si no es la última fila, salto
-=======
     mov x1, 0
     mov x2, 0
     mov x3, 640
@@ -735,7 +715,6 @@ loop0:
     // COLOR #605E48
     movz w10, 0x60, lsl 16
     movk w10, 0x5E48, lsl 00
->>>>>>> main
 
 	// Ejemplo de uso de gpios
 	mov x9, GPIO_BASE
@@ -755,7 +734,6 @@ loop0:
 	// w11 será 1 si había un 1 en la posición 2 de w10, si no será 0
 	// efectivamente, su valor representará si GPIO 2 está activo
 	lsr w11, w11, 1
-
 
 	//---------------------------------------------------------------
 	// Infinite Loop
